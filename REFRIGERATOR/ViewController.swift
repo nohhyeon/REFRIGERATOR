6a9741c9d29fe43dab3db16ec92b9f6420106a62
hyeon su <89899482+nohhyeon@users.noreply.github.com>
2024-06-22 15:30:52 +0900

    Initial Commit

---
+++ REFRIGERATOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  REFRIGERATOR
+//
+//  Created by 노현수 on 6/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
